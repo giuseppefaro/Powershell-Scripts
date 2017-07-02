fbfb44b7d3cc716f031c7973ef039fd820dbd86b
Giuseppe Faro <giuseppe.faro88@gmail.com>
2017-07-02 02:37:58 +0100

    Temperature Implemented

--- ComputerInfo.ps1
+++ ComputerInfo.ps1
@@ -2,6 +2,45 @@
 # Shows details of currently running PC
 
 
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+   }
+else
+   {
+   # We are not running "as Administrator" - so relaunch as administrator
+   
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+   }
+ 
+# Run your code that needs to be elevated here
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
 $computerSystem = Get-CimInstance CIM_ComputerSystem
 $computerBIOS = Get-CimInstance CIM_BIOSElement
 $computerOS = Get-CimInstance CIM_OperatingSystem
@@ -38,6 +77,27 @@ Foreach ($Card in $ComputerVideoCard)
     $Output | Add-Member NoteProperty "$($Card.DeviceID)_Video Card RAM in MB" ($card.AdapterRAM/1MB)
     }
 $Output
+
+function Get-Temperature {
+    $t = @( Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi" )
+    $returntemp = @()
+
+    foreach ($temp in $t)
+    {
+        $currentTempKelvin = $temp.CurrentTemperature / 10
+        $currentTempCelsius = $currentTempKelvin - 273.15
+
+        $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+
+        $returntemp += "Cpu Temperature in C : " + $currentTempCelsius.ToString() 
+    }
+    return $returntemp
+    
+}
+
+Get-Temperature
+echo "Hard Disk Status "
+Get-PhysicalDIsk | Get-StorageReliabilityCounter |  Select-Object DeviceID, Temperature,ReadErrorsTotal
 
 
 write-host -nonewline "press a button to close "
