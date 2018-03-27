0e52622ca6af9cd4b18999676de7469a00bc8d7d
Giuseppe Faro <giuseppe@giuseppefaro.me>
2018-03-27 18:01:41 +0100

    Create Remote.ps1

---
+++ Remote.ps1
@@ -0,0 +1,9 @@
+# This script help you to remote connect to a Windows Machine using Powershell #
+# Author Giuseppe Faro       
+#
+$computer = Read-host "Type the Computer name you want remote into"
+echo ""
+Enter-PSSession –ComputerName $computer -Credential $env:UserDomain\$env:UserName
+
+# Wait for input before closing.
+Read-Host -Prompt “Press Enter to exit”
