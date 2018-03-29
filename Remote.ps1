97caef7edb71cef7fb3b7ed36acdbefc06070d50
Giuseppe Faro <gef@dneg.com>
2018-03-29 15:59:01 +0100

    fix

--- Remote.ps1
+++ Remote.ps1
@@ -6,5 +6,6 @@
 $computer = Read-host "Type the Computer name you want remote into"
 write-host""
 Enter-PSSession -ComputerName $computer -Credential $env:UserDomain\$env:UserName
+
 # Wait for input before closing
 # Read-Host -Prompt "Press Enter to exit"
