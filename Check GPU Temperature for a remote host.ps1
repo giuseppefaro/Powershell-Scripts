5e3df3d2f81781e08fa0bbd8f8a36a99f5b1ae19
Giuseppe Faro <gef@dneg.com>
2018-03-29 15:34:05 +0100

    layout fix

--- Check GPU Temperature for a remote host.ps1
+++ Check GPU Temperature for a remote host.ps1
@@ -1,6 +1,7 @@
 ﻿
 # Common Variable
 $ComputerName= Read-host "Type the hostname of the machine you'd like check "
+Write-Output ""
 
 function Get-Gpu-Temp{
     $video_card_name=(Get-WmiObject win32_videocontroller -ComputerName $ComputerName)|select-object -expand name
@@ -12,20 +13,12 @@ function Get-Gpu-Temp{
     #Write-Host "True"
     Get-WmiObject -ComputerName $ComputerName -Namespace root\CIMV2\NV gpu|select-object name,percent*
  
-    # For local system (e.g. "LocalHost", ".") '-computername $computer' could be omitted
-    # retrieve all instances of the ThermalProbe class and store them:
     $probes = Get-WmiObject -class $classname -computername $ComputerName -namespace $namespace
     # print all ThermalProbe instances
     $probes = $probes.Temperature
     Write-Output ""
     Write-Output "GPU temperature for $Computername is $probes Celsius"
-    # iterate through all Probe instances
-    # foreach( $probe in $probes )
-    # {
-    # #"Call the info() method and print all the data"
-    # $res = $probe.InvokeMethod("info",$Null)
-    # $res
-   
+ 
 }
 Get-Gpu-Temp
 
