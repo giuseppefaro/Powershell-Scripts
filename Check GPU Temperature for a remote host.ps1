407276633fdb7635c649455623e0f1e828d37db4
Giuseppe Faro <gef@dneg.com>
2018-03-29 14:36:11 +0100

    Adding CPU Temperature

---
+++ Check GPU Temperature for a remote host.ps1
@@ -0,0 +1,32 @@
+﻿
+$ComputerName= Read-host "Type the hostname of the machine you'd like check "
+# Common Variable
+$namespace = "root\CIMV2\NV"
+$classname = "ThermalProbe"
+
+function Get-Gpu-Temp{
+    $video_card_name=(Get-WmiObject win32_videocontroller -ComputerName $ComputerName)|select-object -expand name
+    write-host $video_card_name 
+
+    #Write-Host "True"
+    Get-WmiObject -ComputerName $ComputerName -Namespace root\CIMV2\NV gpu|select-object name,percent*
+ 
+    # For local system (e.g. "LocalHost", ".") '-computername $computer' could be omitted
+    # retrieve all instances of the ThermalProbe class and store them:
+    $probes =Get-WmiObject -class $classname -computername $ComputerName -namespace $namespace
+    # print all ThermalProbe instances
+    $probes
+    # iterate through all Probe instances
+    foreach( $probe in $probes )
+    {
+    #"Call the info() method and print all the data"
+    $res = $probe.InvokeMethod("info",$Null)
+    $res
+    #"Query just the temperature"
+    #$temp = $probe.temperature
+    #$temp
+    }
+}
+Get-Gpu-Temp
+# wait for input before closing.
+#Read-Host -Prompt “Press Enter to exit”
